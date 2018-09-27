da12ef452ea9461dbe9cdc92de2d63d5715fd360
swit <swithaoy@gmail.com>
2018-09-27 21:43:12 +0800

    答案比对功能

---
+++ src/main/java/CheckSevice.java
@@ -0,0 +1,93 @@
+import org.apache.commons.math3.fraction.Fraction;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description 检查题目与答案正误逻辑业务类
+ * @Date 2018-09-24
+ * @Time 17:07
+ */
+@SuppressWarnings("unchecked")
+class CheckSevice {
+    //正确题目个数
+    private static int correct = 0;
+    //错误题目个数
+    private static int wrong = 0;
+    //正确题目列表
+    private static List<Integer> correctList = new ArrayList<>();
+    //错误题目列表
+    private static List<Integer> wrongList = new ArrayList<>();
+
+    /**
+     * 比对题目与答案的正确与否
+     *
+     * @param exercisePath 题目文件路径
+     * @param answerPath   答案文件路径
+     */
+    static void checkExercise(String exercisePath, String answerPath) throws IOException {
+        BufferedReader exerciseReader = new BufferedReader(new FileReader(exercisePath));
+        List<String> operatorList = new ArrayList<>(Arrays.asList("+", "-", "×", "÷", "(", ")"));
+        //读取题目文件，并生成题目的答案
+        List<Fraction> realAnswerList = exerciseReader.lines()
+                .map(s -> s.substring(s.indexOf("：") + 1, s.indexOf("=")).trim())
+                .map(s -> s.split("\\s+"))
+                .map(array -> {
+                    List questionList = new ArrayList();
+                    for (String s : array) {
+                        //转换为分数
+                        if (!operatorList.contains(s)) {
+                            questionList.add(Util.getFraction(s));
+                        } else {
+                            questionList.add(s);
+                        }
+                    }
+                    return questionList;
+                })
+                .map(questionList -> ExerciseSevice.getAnswer(questionList))
+                .collect(toList());
+        //读取答案文件
+        BufferedReader answerReader = new BufferedReader(new FileReader(answerPath));
+        List<Fraction> answerList = answerReader.lines()
+                .map(s -> s.substring(s.indexOf(".") + 1).trim())
+                .map(Util::getFraction)
+                .collect(toList());
+        //比对答案与正确答案，生成结果
+        getCheckResult(realAnswerList, answerList);
+        //将结果写入文件中
+        BufferedWriter gradeWriter = new BufferedWriter(new FileWriter(Main.GRADE_PATH));
+        gradeWriter.flush();
+        gradeWriter.write("Correct: " + correct + " " + correctList);
+        gradeWriter.newLine();
+        gradeWriter.write("Wrong: " + wrong + " " + wrongList);
+        //结束，关闭流
+        gradeWriter.close();
+    }
+
+    /**
+     * 比对答案
+     *
+     * @param realAnswerList 正确答案
+     * @param answerList     输入的答案
+     */
+    private static void getCheckResult(List<Fraction> realAnswerList, List<Fraction> answerList) {
+        for (int i = 0; i < realAnswerList.size(); i++) {
+            //题目正确
+            if (realAnswerList.get(i).equals(answerList.get(i))) {
+                correct++;
+                correctList.add(i + 1);
+            } else {
+                //题目错误
+                wrong++;
+                wrongList.add(i + 1);
+            }
+        }
+    }
+}
---
+++ src/main/java/Util.java
@@ -0,0 +1,91 @@
+import org.apache.commons.math3.fraction.Fraction;
+
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description 处理符号处理工具类
+ * @Date 2018-09-24
+ * @Time 17:10
+ */
+class Util {
+    private final static String[] OPERATOR = {"+", "×", "÷", "-"};
+
+    /**
+     * 将数值转换为分数
+     */
+    static Fraction getFraction(String num) {
+        //带分数
+        if (num.contains("'")) {
+            int index1 = num.indexOf("'");
+            int index2 = num.indexOf("/");
+            return new Fraction(Integer.valueOf(num.substring(0, index1)) *
+                    Integer.valueOf(num.substring(index2 + 1, num.length())) +
+                    Integer.valueOf(num.substring(index1 + 1, index2)),
+                    Integer.valueOf(num.substring(index2 + 1, num.length())));
+            //分数
+        } else if (num.contains("/")) {
+            int index = num.indexOf("/");
+            return new Fraction(Integer.valueOf(num.substring(0, index)), Integer.valueOf(num.substring(index + 1)));
+            //整数
+        } else {
+            return new Fraction(Integer.valueOf(num));
+        }
+    }
+
+    /**
+     * 生成真分数/带分数
+     *
+     * @param range 取值范围
+     */
+    static Fraction getFraction(int range) {
+        Random random = new Random();
+        //分母
+        int denominator = random.nextInt(9) + 2;
+        //分子
+        int numerator = random.nextInt(denominator - 1) + 1;
+        int commonFactor = 1;
+        //约分
+        for (int i = 1; i <= numerator; i++) {
+            if (denominator % i == 0 && numerator % i == 0) {
+                commonFactor = i;
+            }
+        }
+        denominator = denominator / commonFactor;
+        numerator = numerator / commonFactor;
+        //带分数与真分数
+        if (random.nextBoolean()) {
+            int integer = random.nextInt(range) + 1;
+            return new Fraction(integer * denominator + numerator, denominator);
+        } else {
+            return new Fraction(numerator, denominator);
+        }
+    }
+
+    /**
+     * 生产四则运算符
+     */
+    static String getOperator() {
+        return OPERATOR[(int) (Math.random() * 4)];
+    }
+
+    /**
+     * 将 Fraction 类型格式化
+     */
+    static Object getNum(Fraction fraction) {
+        int numerator = fraction.getNumerator();
+        int denominator = fraction.getDenominator();
+        int integer;
+        if (denominator == 1) {
+            return numerator;
+        } else if (numerator < denominator) {
+            return numerator + "/" + denominator;
+        } else {
+            integer = numerator / denominator;
+            numerator = numerator - integer * denominator;
+            return integer + "'" + numerator + "/" + denominator;
+        }
+    }
+}
